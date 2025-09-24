6ebf1041927c54d330e273538904df75f887018d
hm32295 <hm32295@gmail.com>
2025-09-24 09:36:50 +0300

    type

--- src/components/loading/spinnerComponent.tsx
+++ src/components/loading/spinnerComponent.tsx
@@ -4,7 +4,7 @@ interface typeSize{
     md: "h-6 w-6";
     lg: "h-10 w-10";
 }
-export default function Spinner({ size = "md" }:{size:'sm' | "md" | 'lg'}) {
+export default function Spinner({ size = "md" }:{size?:'sm' | "md" | 'lg'}) {
   const sizes:typeSize = {
     sm: "h-4 w-4",
     md: "h-6 w-6",
